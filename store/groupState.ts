ee8fcf39101d9edc0f1b56d90c65429cb8a3d09a
thekogo <thekogo@gmail.com>
2023-03-13 04:32:49 +0700

    fix default type

--- store/groupState.ts
+++ store/groupState.ts
@@ -3,5 +3,5 @@ import { getGroupsByUserId } from "~/services/models/group.server";
 
 export const groupState = atom({
   key: 'groupState',
-  default: {} as Awaited<ReturnType<typeof getGroupsByUserId>>
+  default: [] as Awaited<ReturnType<typeof getGroupsByUserId>>
 })
